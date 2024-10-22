e005ab2bbf91d8af24d5d3a03295fa18aab9f027
EarthSR <earthsrichok31@gmail.com>
2024-10-22 10:00:10 +0700

    setup

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,4 +51,9 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    implementation("com.squareup.okhttp3:okhttp:4.12.0")
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+
 }
